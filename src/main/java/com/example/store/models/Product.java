65aa1743ef83b5960547d6fe9c5de3703086b5d1
Arsen0205 <arsem.murtazayev@inbox.ru>
2024-11-08 13:26:55 +0300

    three  commit

---
+++ src/main/java/com/example/store/models/Product.java
@@ -0,0 +1,53 @@
+package com.example.store.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
