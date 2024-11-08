d3576ac535fdce0c3e2ade7ab54b558f0e05180a
Arsen0205 <arsem.murtazayev@inbox.ru>
2024-11-08 13:06:48 +0300

    first commit

---
+++ src/main/java/com/example/store/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.example.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Класс, отвечающий за прием HTTP-запросов
+@Controller
+public class ProductController {
+    @GetMapping("/")
+    public String showProductsForm(){
+        return "products";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test"; // файл test.html должен быть в папке templates
+    }
+}
